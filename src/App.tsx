c1fa3b834d9cea687b4eb85a8e8f21e9e540ee06
getparas <karkiparas020@gmail.com>
2025-12-04 20:44:32 +0545

    simple dynamic form with tanstack form & zod

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import DynamicFormExample from "@/components/dynamic-form-example";
+
+function App() {
+  return (
+    <main>
+      <DynamicFormExample />
+    </main>
+  );
+}
+
+export default App;
